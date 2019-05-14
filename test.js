dc613ed760f7901ae62178dd486cb1da83a52ad4
Wioletta Kilimnik <wiolettakilimnik@gmail.com>
2019-05-14 18:01:06 +0200

    JS Basic Rules

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
+// the first alert
 alert("Hello,World!");
+
+
+/* Summary : 
+JS is case sensitive
+semicolon on ending
+not sensitive to whitespace
+oneline comment //
+multi comment like in CSS
+runs from top to bottom */ 
