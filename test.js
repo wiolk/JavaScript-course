07be4f5191cb840a2edaf1cf22c260d6d9797f22
Wioletta Kilimnik <wiolettakilimnik@gmail.com>
2019-05-14 18:53:17 +0200

    Else If Statements - tested

--- test.js
+++ test.js
@@ -1,11 +1,18 @@
-var youLikeMeat = true;
-var myNum = 8;
+var myAge = 32;
 
-if (youLikeMeat){
+if(myAge > 30){
 
-    document.write("you like meat");
+    document.write("you are over 30");
 
+} else if (myAge > 20){
+
+    document.write("you are over 20");
+
+} else if (myAge > 10){
+
+    document.write("you are over 10!");
 } else{
-    document.write("you hate meat")
+
+    document.write("you are a kid!");
 
 }
