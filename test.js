b3df960438c146c0a929e394078589a6e4875945
Wioletta Kilimnik <wiolettakilimnik@gmail.com>
2019-05-15 09:32:04 +0200

    For Loops - tested

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-var age = 5;
 
-while (age < 10) {
+var links = document.getElementsByTagName("a");
 
-    console.log("Your age is less than 10");
-    age++;
 
+for (i = 0; i <= links.length; i++) {
+
+    console.log("this is link number" + i);
+    
 }
 
-document.write("you are over 10");
+document.write("all links now looped");
