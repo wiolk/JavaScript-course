4e2f9eb1b60f290d1fdb0c630410df447cd6bb6a
Wioletta Kilimnik <wiolettakilimnik@gmail.com>
2019-05-15 11:25:22 +0200

    Arrays - tested

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
-var string1 = "abc";
-var string2 = "Bcd";
+var myArray = [];
+myArray[0] = 25;
+myArray[1] = 35;
+myArray[2] = true;
+myArray[3] = "hello";
 
-console.log(string1 < string2);
 
+myArray[2] = false;
 
+//console.log(myArray);
 
+var myArray2 = [10,20,"hi",false];
+//console.log(myArray2);
+
+var myArray3 = new Array();
+
+var myArray4 = new Array(5);
+
+console.log(myArray2.reverse());
