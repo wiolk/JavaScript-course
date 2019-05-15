0b8897112ed93fe336c073e29ab46e0dd4e56854
Wioletta Kilimnik <wiolettakilimnik@gmail.com>
2019-05-15 11:58:30 +0200

    THIS keyword tested

--- test.js
+++ test.js
@@ -17,8 +17,14 @@ var myCar2 = {
     driver: "Cat Woman", 
     drive: function(speed, time){
         console.log(speed * time);
-    } 
+    },
+    logDriver: function(){
+        console.log("driver name is " + this.driver);
+    }
 };
 
+
+
+myCar2.logDriver();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
